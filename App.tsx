216f4718241ce123e29968cece5453d2bda9c280
Granth Gharewal <gharewal12@gmail.com>
2023-01-26 17:24:11 +0530

    Added different touchable effects

--- App.tsx
+++ App.tsx
@@ -1,15 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View ,SafeAreaView} from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback } from "react-native";
+import favicon from "./assets/favicon.png";
 
 export default function App() {
 
-  const handlePress=()=>{
+  const handlePress = () => {
     console.log("Text clicked");
-  }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}>Hello React Native. This is a veru long text to test numberoflines props </Text>
+      <Image source={favicon} />
+      <Text>Please click on the below image to see the effect.</Text>
+      <TouchableHighlight onPress={() => console.log("Image is clicked")}>
+        <Image
+          source={{
+            uri: "https://picsum.photos/200",
+            width: 200,
+            height: 300,
+          }} />
+      </TouchableHighlight>
+      <TouchableNativeFeedback onPress={() => console.log("image tapped")}>
+        <View style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}></View>
+      </TouchableNativeFeedback  >
       <StatusBar style="auto" />
     </SafeAreaView>
   );
